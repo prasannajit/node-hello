13f9febcee85bbd507740f73433976522988c24d
Prasannajit Tripathy <prasannajit.tripathy@tesco.com>
2019-09-09 20:20:42 +0530

    updated port

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const http = require('http');
 // file.pipe(file1).on('close',()=>{
 //   console.log('done');
 // });
+const port = process.env.PORT || 3000;
 const reqHandler = (req,res)=>{
     const file = fs.createReadStream('./demo.html');
     file.pipe(res);
 };
 const server = http.createServer(reqHandler);
-server.listen(3000,()=>{
-    console.log("server listening on 3000");
+server.listen(port,()=>{
+    console.log(`server listening on ${port}`);
 })
